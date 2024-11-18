e243847d90ca08d5cb9c0c2a09fb7341226484d5
tosuiEpitech <thomas.pozzi@epitech.eu>
2024-11-18 11:16:15 +0100

    fixed patrolling behavior of NPCs and rearranged the structure of the NPC class

--- SKS/src/states/PatrollingNPC.java
+++ SKS/src/states/PatrollingNPC.java
@@ -2,60 +2,67 @@ package states;
 
 import entities.NPCs;
 import levels.LevelManager;
+import main.Game;
+import static utils.Methods.canMoveHere;
+import static utils.Constants.EnemyConstants.*;
+import static utils.Methods.isEntityOnFloor;
 
 public class PatrollingNPC extends NPCs {
-   
-    public PatrollingNPC(int x, int y, String name, LevelManager levelManager) {
-        super(x, y, 50, 100, name, levelManager);
-        this.currentState = NPC_state.PATROL;
+
+    public PatrollingNPC(float x, float y, int width, int height, String name, LevelManager levelManager) {
+        super(x, y, width, height, name, levelManager);
+        initHitBox(x, y, 18 * Game.SCALE * NPC_SCALE, 25 * Game.SCALE * NPC_SCALE);
     }
 
-    @Override
-    public void update() {
-        if (currentState == NPC_state.PATROL) {
-            startPatrol();
+//    public void switchDirection() {
+//        if (currentDirection == Direction.RIGHT) {
+//            currentDirection = Direction.LEFT;
+//        }
+//        else if (currentDirection == Direction.LEFT) {
+//            currentDirection = Direction.RIGHT;
+//        }
+//    }
+
+    protected void updateMove() {
+        while (!isEntityOnFloor(hitBox, levelManager.getCollisionMap())) {
+            hitBox.y += 1;
+        }
+        elapsedTime = System.nanoTime() - startTime;
+
+        System.out.println("Current Action: " + getNPCAction() + ", Elapsed Time: " + elapsedTime);
+
+        switch (getNPCAction()) {
+            case IDLE:
+                if (elapsedTime > 5000000000L) { // Wait for 3 seconds
+                    switchDirection();
+                    newState(WALK);
+                    startTime = System.nanoTime();
+                }
+                break;
+            case WALK:
+                if (elapsedTime > 2000000000L) { // Patrol for 1 second
+                    newState(IDLE);
+                    startTime = System.nanoTime();
+                } else {
+                    if (currentDirection == Direction.RIGHT) {
+                        if (canMoveHere(hitBox.x + speed, hitBox.y, hitBox.width, hitBox.height, levelManager.getCollisionMap())) {
+                            hitBox.x += speed * 0.2;
+                        } else {
+                            switchDirection();
+                            startTime = System.nanoTime();
+                        }
+                    } else {
+                        if (canMoveHere(hitBox.x - speed, hitBox.y, hitBox.width, hitBox.height, levelManager.getCollisionMap())) {
+                            hitBox.x -= speed * 0.2;
+                        } else {
+                            switchDirection();
+                            startTime = System.nanoTime();
+                        }
+                    }
+                }
+                break;
         }
     }
 
-    private void startPatrol() {
-        elapsedTime = (System.nanoTime() - startTime) / 1000000;
-        if (elapsedTime > nextAction) {
-            patrolLR();
-            switchDirection();
-            switchState();
-        }
 
-    }
-
-    public void switchDirection() {
-        if (currentDirection == Direction.RIGHT) {
-            currentDirection = Direction.LEFT;
-        } 
-        else if (currentDirection == Direction.LEFT) {
-            currentDirection = Direction.RIGHT;
-        }
-    }
-
-    public void switchState() {
-        if (currentState == NPC_state.IDLE) {
-            currentState = NPC_state.PATROL;
-            isMoving = false;
-            nextAction = 3000;
-        }
-        else if (currentState == NPC_state.PATROL) {
-            currentState = NPC_state.IDLE;
-            isMoving = true;
-            nextAction = 4000;
-        }
-    }
-
-    public void patrolLR() {
-        if (currentState == NPC_state.PATROL) {
-            if (currentDirection == Direction.RIGHT) {
-            x += speed;
-        } else {
-            x -= speed;
-        }
-        }
-    }
 }
