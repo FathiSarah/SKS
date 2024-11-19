8d19cde23546ce905f42b6590a64b0aa2f04bcc4
tropicalcroissant <sarah.fathi@epitech.eu>
2024-11-19 14:10:38 +0100

    implementation of gamestates, switch menu and gamestate, blank menu

--- SKS/src/main/Game.java
+++ SKS/src/main/Game.java
@@ -1,6 +1,7 @@
 package main;
 
 
+import Gamestates.Gamestate;
 import entities.NPCs;
 import levels.LevelBase;
 import levels.LevelFactory;
@@ -89,8 +90,18 @@ public class Game implements Runnable {
      * Update the game logic (player/player's hitbox position, animation sprites, etc).
      */
     public void update(){
-        player.update();
-        activeLevel.update(player);
+
+        switch(Gamestate.state) {
+            case MENU:
+
+                break;
+            case PLAYING:
+                player.update();
+                activeLevel.update(player);
+                break;
+            default:
+                break;
+        }
     }
 
     public void loadLevel(int LevelNumber) {
@@ -104,9 +115,17 @@ public class Game implements Runnable {
      * @param g
      */
     public void render( Graphics g){
-       activeLevel.render(g);
+        switch(Gamestate.state) {
+            case MENU:
+                break;
+            case PLAYING:
+                activeLevel.render(g);
+                player.render(g);
+                break;
+            default:
+                break;
+        }
 
-       player.render(g);
     }
 
 
